4530cbb16b55ca91427e4f437d69e8677b0c8708
Pawan Gola <pawangola908@gmail.com>
2024-05-20 14:32:15 +0530

    conversion operations

--- 03_coversionOperator.js
+++ 03_coversionOperator.js
@@ -1,28 +1,57 @@
-let score = "33abc";
-// let score = null;
-// let score = undefined;
-// let score = "keshav";
-// let score =  true;
+// let score = "33abc";
+// // let score = null;
+// // let score = undefined;
+// // let score = "keshav";
+// // let score =  true;
 
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-//1 => true; 0=> false
-//" " => false
-// "hitesh " => true 
+// //1 => true; 0=> false
+// //" " => false
+// // "hitesh " => true 
 
 
-let isLoggedIn = "keshav"
+// let isLoggedIn = "keshav"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-let someNumber = 23
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 23
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//************** Opertions  ********************** */
+
+
+// let value = 3;
+// let nagvalue = -value
+// console.log(nagvalue);
+
+// let mValue = 10;
+// let newvalue = -mValue; 
+// console.log(newvalue);
+
+
+// let str1 = "hello"
+// let str2 = "hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(2 + 1 + "2");
+// console.log("2" + 1 + 2);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
