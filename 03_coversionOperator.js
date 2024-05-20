8af271dcef34cdf0205ddb9c0ee8ffda8fb265b9
Pawan Gola <pawangola908@gmail.com>
2024-05-20 14:10:17 +0530

    conversion of datatypes

---
+++ 03_coversionOperator.js
@@ -0,0 +1,28 @@
+let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = "keshav";
+// let score =  true;
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//1 => true; 0=> false
+//" " => false
+// "hitesh " => true 
+
+
+let isLoggedIn = "keshav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 23
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 console.log("Keshav");
+
+// alert(3+3); 
+
+const age = 34; 
+
+console.log(typeof age);
+console.log(typeof null);  
