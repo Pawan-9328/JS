db1b12ff55759d974a3c2b030b41457b6a5566bc
Pawan Gola <pawangola908@gmail.com>
2024-05-20 15:53:42 +0530

    stack and heap memory concept

--- 04_comparision.js
+++ 04_comparision.js
@@ -1,12 +1,34 @@
-console.log("2" > 1);
-console.log( "02" > 1);
+// console.log("2" > 1);
+// console.log( "02" > 1);
 
 
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
-console.log(undefined > 0);
-console.log(undefined == 0);
-console.log(undefined >= 0);
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// ****** Stack  and Heap Memory ******* 
+
+let myYoutubename = "isKeshav"
+let  anotherName = myYoutubename
+
+anotherName = "isPawan"
+
+console.log(anotherName);
+
+
+let userOne = {
+      email: "user@gmail.com",
+      upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "keshav@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
