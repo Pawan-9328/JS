110ea267c2982d3c1e139fed010eeb82215b8c43
Pawan Gola <pawangola908@gmail.com>
2024-05-20 15:36:34 +0530

    data type summary

---
+++ data_types-summary.js
@@ -0,0 +1,37 @@
+// Primitive 
+
+
+//7 types: String, Number, Boolean, null , undefined, Symbol  
+
+// const score = 100;
+// const scoreValue = 1200.3;
+// const isLoggedIn = false
+// const outsideTemp = null;
+// let userEmail;
+
+// const id = Symbol('123');
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigInteger = 266666666377777844;
+
+// Reference (Non primitive)
+
+// Array, Objects , Functions 
+
+const heros = ["shaktiman", "number", "hello"];
+
+let myObj = {
+   name: "hitesh",
+   age: 22
+}
+
+
+const myFunction= function(){
+     console.log("Hello everyone");
+}
+
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof myFunction);
