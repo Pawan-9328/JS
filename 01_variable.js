3bb1f12890d9609474419b572f39bc538d512309
Pawan Gola <pawangola908@gmail.com>
2024-05-20 13:07:07 +0530

    perform simple variable operation

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "keshav@google.com"
+var accountpassword = "1234"
+accountCity = "Delhi";
+
+
+//accountId = 2; 
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountpassword,accountCity])
