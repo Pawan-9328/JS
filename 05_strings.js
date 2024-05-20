f86fd961ceb52ef325ab11a8820ae8c517dbb087
Pawan Gola <pawangola908@gmail.com>
2024-05-20 17:31:58 +0530

    strings concepts

---
+++ 05_strings.js
@@ -0,0 +1,39 @@
+const name = "Naman"
+const repoCount = 30
+
+// ...this is outdated syntex......
+// console.log(name + repoCount+ "Value");
+
+
+//.....modern way called string intter-pollitions ........
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hiteshhc');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// // check character positions 
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('t'));
+
+
+// const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "  pawan "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "htttp:///keshav.com";
+// console.log(url.replace('.com', '.in'));
+
+// console.log(url.includes(''));
+
+console.log(gameName.split('-'));
