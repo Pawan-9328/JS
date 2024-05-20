4692c69ced5030e5933ca6d6f6c23a69474d2275
Pawan Gola <pawangola908@gmail.com>
2024-05-20 18:55:45 +0530

    perform math and nums opertion

---
+++ 06_nums_and_math.js
@@ -0,0 +1,31 @@
+const score = 400
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+// const hunderds = 100000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+//**************  Maths  *************** */
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+
+const min = 10;
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
