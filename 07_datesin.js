42e0cfd6b23028346da8e9ceeb14b8248720c535
Pawan Gola <pawangola908@gmail.com>
2024-05-21 16:17:25 +0530

    add array methods part - 1

---
+++ 07_datesin.js
@@ -0,0 +1,14 @@
+//........Dates...........
+
+
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+// let myCraetedDate = new Date(2023, 0, 23)
+let myCraetedDate = new Date("2023-01-14")
+console.log(myCraetedDate.toLocaleString());
+// console.log(myCraetedDate);
