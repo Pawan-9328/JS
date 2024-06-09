5ed20029e727f7a4fbc0a4c889186d045d2e22ce
Pawan Gola <pawangola908@gmail.com>
2024-06-10 01:21:14 +0530

    objest second part

---
+++ 010_object_sec.js
@@ -0,0 +1,56 @@
+//const tinderUser = new Object()
+
+
+const tinderUser = {};
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sunny"
+tinderUser.isLoggedIn = false;
+
+
+// console.log(tinderUser);
+
+//.....object under objects.....
+
+// const regulaUser = {
+//    email: "some@gmail.com",
+//    fullName: {
+//       userfullname: {
+//          firstname: "Keshav",
+//          lastname: "Gola"
+//       }
+//    }
+// }
+
+// console.log(regulaUser.fullName?.userfullname.firstname);
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a" , 6: "b"}
+
+// const obj3 = Object.assign({} , obj1, obj2, obj4); 
+// console.log(obj3);
+
+// array objects 
+
+const users = [
+    {
+
+    },
+    {
+
+    },
+    {
+
+    },
+
+
+]
+
+users[1].email 
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(Object.hasOwnProperty('isLoggedIn'));
---
+++ 09_Objects.js
@@ -0,0 +1,19 @@
+
+//...singleton 
+
+// .....object literals.....
+
+const JsUser = {
+   name: "keshav",
+   "full name": "keshav Num",
+   age: 18,
+   location: "Delhi",
+   email: "keshav@gmail.com",
+   isLoggedIn: false,
+   lastLoginDays: ["Monday", "Saturday"]
+
+
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
