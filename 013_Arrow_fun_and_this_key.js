2bd1f6e894d9d10a61e3557618c182265dc37d28
Pawan Gola <pawangola908@gmail.com>
2024-06-10 13:59:51 +0530

    arrow fucntion example

--- 013_Arrow_fun_and_this_key.js
+++ 013_Arrow_fun_and_this_key.js
@@ -1,24 +1,52 @@
 
-const user = {
-   username: "keshav",
-   price: 99,
+// const user = {
+//    username: "keshav",
+//    price: 99,
 
-   // this - refer to the current object 
-   welcomeMessage: function () {
-      console.log(`${this.username}, welcome to website`);
-   }
-}
+//    // this - refer to the current object 
+//    welcomeMessage: function () {
+//       console.log(`${this.username}, welcome to website`);
+//    }
+// }
    
-// user.welcomeMessage();
-// //here we change context 
-// user.username = "num"
-// user.welcomeMessage()
+// // user.welcomeMessage();
+// // //here we change context 
+// // user.username = "num"
+// // user.welcomeMessage()
 
-//console.log(this);
+// //console.log(this);
 
-function chai(){
-    let username = "hitesh"
-    console.log(this.username);
-}
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
 
-chai(); 
+// chai(); 
+
+
+// const chai = () =>{
+//      let username = "hitesh"
+//      console.log(this);
+// }
+
+// chai(); 
+
+// const addTwo = (num1 , num2) =>{
+//      return num1 + num2
+// }
+
+// console.log(addTwo(4,7)); 
+
+//implicit return 
+//const addTwo = (num1 , num2) => num1 + num2
+
+//....return objects......but object writs in parennthesis  
+
+const addTwo = (num1 , num2) => ({username: "keshav"}); 
+
+console.log(addTwo(4,7)); 
+
+// using in loop case  
+const myArray = [2,3,4,4,67,8];
+myArray.forEach(()=> {});
+//myArray.forEach(()=> ())
