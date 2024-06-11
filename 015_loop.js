2a10fbbe11108f022c94c2eecdcbd1107d9f38c2
Pawan Gola <pawangola908@gmail.com>
2024-06-11 12:37:04 +0530

    looping concepts

---
+++ 015_loop.js
@@ -0,0 +1,41 @@
+//.......Array specific loop........
+
+
+// const arr = [1, 2, 3, 4, 5];
+
+// for (const num of arr) {
+
+//    console.log(num);
+
+// }
+
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//      console.log(`Each char is ${greet}`);
+// }
+
+//Maps 
+
+const map = new Map() 
+map.set('IN', "india")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+//console.log(map);
+
+// for loop
+
+// for(const [key, value] of map){
+//    console.log(key, ':-' , value);
+// }
+
+//object case 
+
+// const myObject = {
+//       'game1' : 'NFS',
+//        'game2': 'spiderman'
+// }
+// for(const [key ,value] of myObject) {
+//    console.log(key, ':-' , value);
+    
+// }
